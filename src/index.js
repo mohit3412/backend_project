8fb7017369281a12fe1e708f6b05f9328d70f943
Mohit <mohitjangid3412@gmail.com>
2025-08-26 13:26:46 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/database.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -9,6 +10,14 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at Port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MognoDB connection Failed !!: ", error)
+})
 
 
 
